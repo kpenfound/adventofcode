6e3086f05be27f01004c4421395139c31ddb01d1
kpenfound <kyle@dagger.io>
2022-12-02 14:39:23 -0500

    2022.01 pt 2

--- 2022/01/script.py
+++ 2022/01/script.py
@@ -2,22 +2,27 @@ import anyio
 import dagger
 
 async def main():
-  max = 0
+  top = [0, 0, 0]
   args = ['expr', '0']
   async with dagger.Connection() as client:
     base = client.container().from_('alpine')
     with open('input.txt', 'r') as _file:
       for line in _file:
-        if line == "\n":
+        if line.rstrip() == "":
           elf = base.with_exec(args)
           total = int(await elf.stdout())
-          if total > max:
-            max = total
+          if total > top[0]:
+            top[0] = total
+            top.sort()
           args = ['expr', '0']
         else:
           args.append('+')
           args.append(line.rstrip())
-  print('answer is', max)
+    elf = base.with_exec(args)
+    total = int(await elf.stdout())
+    if total > top[0]:
+      top[0] = total
+  print('answer is', sum(top))
 
 if __name__ == "__main__":
   anyio.run(main)
